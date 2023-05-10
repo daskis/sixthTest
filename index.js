9ce39484f7e57f93d92c7de95cc300ece990c172
Anton Zhuravlev <abramoff.job@gmail.com>
2023-05-10 05:33:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express  from "express";
+import mysql from "mysql"
+import bcrypt, { hash } from "bcrypt"
+const PORT = 2001;
+const app = express();
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    database: "node_basics",
+    password: "Daskis009"
+});
+app.post("/test", (req, res) => {
+    bcrypt.hash("admin", 10, (hash) => {
+        connection.query(`INSERT INTO admin (login, password) values ("login", "${password}");`)
+    })
+})
+app.listen(PORT, () => {
+    console.log("working")
+})
